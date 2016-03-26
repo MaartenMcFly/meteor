f1fdef5ed5b437806c8830d2ca7a86979b0572ed
Maarten Smeets | Twinfield <msmeets@TWFNL0494.local>
2016-03-26 20:54:53 +0100

    First commit

---
+++ meteor.js
@@ -0,0 +1,42 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+	if (Session.get("hideCompleted")) {
+		return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});} else {
+		return Tasks.find({}, {sort: {createdAt: -1}}); 
+	}
+    },
+    hideCompleted: function() {
+	return Session.get("hideCompleted")
+    }
+  });
+
+	
+  Template.body.events({
+    	"submit .new-task": function (event) {
+		event.preventDefault();
+		var text = event.target.text.value;
+		Tasks.insert({
+			text: text,
+			createdAt: new Date()
+		});
+
+		event.target.text.value = "";
+	},
+	"click .toggle-checked": function () {
+		Tasks.update(this._id, {
+			$set: {checked: ! this.checked}
+		});
+	},
+	"change .hide-completed input": function (event) {
+		Session.set("hideCompleted", event.target.checked);
+	},
+	"click .delete": function () {
+		Tasks.remove(this._id);
+		}
+	});
+	
+}
